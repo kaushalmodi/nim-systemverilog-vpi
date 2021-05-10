9f6dc7ca5239885a95a230b4248b48b2443a916d
Kaushal Modi <kaushal.modi@gmail.com>
2021-05-10 02:21:05 -0400

    Add VPI application show_value in Nim
    Need to figure out why vpi_control(vpiFinish) is not causing Verilog $finish to
    trigger

---
+++ show_value/libvpi.nim
@@ -0,0 +1,75 @@
+import std/[strformat]
+import svvpi
+
+proc show_value() =
+  proc compiletfShowValue(s: cstring): cint {.cdecl.} =
+    # Obtain a handle to the system task instance.
+    let
+      systfHandle = get_vpi_handle(vpiSysTfCall, nil)
+    if systfHandle == nil:
+      vpiEcho("ERROR: $show_value failed to obtain systf handle")
+      # FIXME: -- Mon May 10 02:17:38 EDT 2021 - kmodi
+      # vpi_control doesn't seem to work
+      # return vpi_control(vpiFinish, 1)
+      return tf_dofinish()
+
+    # Obtain handles to system task arguments.
+    let
+      argIterator = vpi_iterate(vpiArgument, systfHandle)
+    if argIterator == nil:
+      vpiEcho("ERROR: $show_value requires 1 argument")
+      # FIXME: -- Mon May 10 02:17:38 EDT 2021 - kmodi
+      # vpi_control doesn't seem to work
+      # return vpi_control(vpiFinish, 1)
+      return tf_dofinish()
+
+    # Check the type of object in system task arguments.
+    var
+      argHandle = vpi_scan(argIterator)
+    let
+      argType = vpi_get(vpiType, argHandle)
+    if argType notin {vpiNet, vpiReg}:
+      vpiEcho("ERROR: $show_value arg must be a net or reg")
+      discard vpi_free_object(argIterator) # free iterator memory
+      # FIXME: -- Mon May 10 02:17:38 EDT 2021 - kmodi
+      # vpi_control doesn't seem to work
+      # return vpi_control(vpiFinish, 1)
+      return tf_dofinish()
+
+    # Check that there are no more system task arguments.
+    argHandle = vpi_scan(argIterator)
+    if argHandle != nil:
+      vpiEcho("ERROR: $show_value can only have 1 argument")
+      discard vpi_free_object(argIterator) # free iterator memory
+      # FIXME: -- Mon May 10 02:17:38 EDT 2021 - kmodi
+      # vpi_control doesn't seem to work
+      # return vpi_control(vpiFinish, 1)
+      return tf_dofinish()
+
+  proc calltfShowValue(s: cstring): cint {.cdecl.} =
+    # Obtain a handle to the system task instance.
+    let
+      systfHandle = get_vpi_handle(vpiSysTfCall, nil)
+
+    # Obtain handle to system task argument.  compiletf has already
+    # verified only 1 arg with correct type.
+    let
+      argIterator = vpi_iterate(vpiArgument, systfHandle)
+      netHandle = vpi_scan(argIterator)
+    discard vpi_free_object(argIterator) # free iterator memory
+
+    # Read current value.
+    var
+      currentValue = s_vpi_value(format: vpiBinStrVal) # read value as a string
+    vpi_get_value(netHandle, addr currentValue)
+    vpiEcho &"Signal {vpi_get_str(vpiFullName, netHandle)} has the value {currentValue.value.str}"
+
+  var
+    taskDataObj = s_vpi_systf_data(`type`: vpiSysTask,
+                                   tfname: "$show_value",
+                                   compiletf: compiletfShowValue,
+                                   calltf: calltfShowValue,
+                                   sizetf: nil)
+  discard vpi_register_systf(addr taskDataObj)
+
+setVlogStartupRoutines(show_value)
---
+++ show_value/tb.sv
@@ -0,0 +1,40 @@
+// Time-stamp: <2021-05-10 02:20:25 kmodi>
+
+`timescale 1ns/1ns
+
+module addbit(a, b, ci, sum, co);
+  input a, b, ci ;
+  output sum, co;
+  wire a, b, ci, sum,
+       ni, n2, n3;
+  xor (ni, a, b) ;
+  xor #2 (sum, ni, ci) ;
+  and (n2, a, b) ;
+  and (n3, ni, ci) ;
+  or #2 (co, n2, n3);
+endmodule : addbit
+
+module top;
+
+  reg a, b, ci, clk;
+  wire sum, co;
+  addbit il (a, b, ci, sum, co);
+
+  initial begin
+    clk = 0;
+    a = 0;
+    b = 0;
+    ci = 0;
+    #10 a = 1;
+    #10 b = 1;
+    // $show_value(); // will cause compilation error
+    // $show_value(sum, 123); // will cause compilation error
+    // $show_value(123); // will cause compilation error
+    $show_value(sum);
+    $show_value(co);
+    $show_value(il.n3);
+    // #10 $stop;
+    $finish;
+  end // initial begin
+
+endmodule : top
