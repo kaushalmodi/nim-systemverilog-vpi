b8f4a4b491bb02e78238ee7ed8cd6ce5c5acd5a0
Kaushal Modi <kaushal.modi@gmail.com>
2021-05-13 09:26:00 -0400

    Revert "Update for changes to svvpi v0.0.2"
    This reverts commit 0ee81e20517c83766fa5c5097842f1fc8885e2a1.

--- show_value/libvpi.nim
+++ show_value/libvpi.nim
@@ -5,7 +5,7 @@ proc show_value() =
   proc compiletfShowValue(s: cstring): cint {.cdecl.} =
     # Obtain a handle to the system task instance.
     let
-      systfHandle = getVpiHandle(vpiSysTfCall, nil)
+      systfHandle = vpi_handle(vpiSysTfCall, nil)
     if systfHandle == nil:
       vpiEcho("ERROR: $show_value failed to obtain systf handle")
       vpiQuit()
@@ -13,7 +13,7 @@ proc show_value() =
 
     # Obtain handles to system task arguments.
     let
-      argIterator = vpiIterate(vpiArgument, systfHandle)
+      argIterator = vpi_iterate(vpiArgument, systfHandle)
     if argIterator == nil:
       vpiEcho("ERROR: $show_value requires 1 argument")
       vpiQuit()
@@ -21,47 +21,47 @@ proc show_value() =
 
     # Check the type of object in system task arguments.
     var
-      argHandle = vpiScan(argIterator)
+      argHandle = vpi_scan(argIterator)
     let
-      argType = vpiGet(vpiType, argHandle)
+      argType = vpi_get(vpiType, argHandle)
     if argType notin {vpiNet, vpiReg}:
       vpiEcho("ERROR: $show_value arg must be a net or reg")
-      discard vpiFreeObject(argIterator) # free iterator memory
+      discard vpi_free_object(argIterator) # free iterator memory
       vpiQuit()
       return
 
     # Check that there are no more system task arguments.
-    argHandle = vpiScan(argIterator)
+    argHandle = vpi_scan(argIterator)
     if argHandle != nil:
       vpiEcho("ERROR: $show_value can only have 1 argument")
-      discard vpiFreeObject(argIterator) # free iterator memory
+      discard vpi_free_object(argIterator) # free iterator memory
       vpiQuit()
       return
 
   proc calltfShowValue(s: cstring): cint {.cdecl.} =
     # Obtain a handle to the system task instance.
     let
-      systfHandle = getVpiHandle(vpiSysTfCall, nil)
+      systfHandle = vpi_handle(vpiSysTfCall, nil)
 
     # Obtain handle to system task argument.  compiletf has already
     # verified only 1 arg with correct type.
     let
-      argIterator = vpiIterate(vpiArgument, systfHandle)
-      netHandle = vpiScan(argIterator)
-    discard vpiFreeObject(argIterator) # free iterator memory
+      argIterator = vpi_iterate(vpiArgument, systfHandle)
+      netHandle = vpi_scan(argIterator)
+    discard vpi_free_object(argIterator) # free iterator memory
 
     # Read current value.
     var
       currentValue = s_vpi_value(format: vpiBinStrVal) # read value as a string
-    vpiGetValue(netHandle, addr currentValue)
-    vpiEcho &"Signal {vpiGet_str(vpiFullName, netHandle)} has the value {currentValue.value.str}"
+    vpi_get_value(netHandle, addr currentValue)
+    vpiEcho &"Signal {vpi_get_str(vpiFullName, netHandle)} has the value {currentValue.value.str}"
 
   var
-    taskDataObj = s_vpiSystf_data(`type`: vpiSysTask,
+    taskDataObj = s_vpi_systf_data(`type`: vpiSysTask,
                                    tfname: "$show_value",
                                    compiletf: compiletfShowValue,
                                    calltf: calltfShowValue,
                                    sizetf: nil)
-  discard vpiRegisterSystf(addr taskDataObj)
+  discard vpi_register_systf(addr taskDataObj)
 
 setVlogStartupRoutines(show_value)
