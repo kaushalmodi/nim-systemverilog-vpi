b2a703dc44d7a1de5252b84aa47b778b672fa316
Kaushal Modi <kaushal.modi@gmail.com>
2021-05-24 00:15:29 -0400

    count_args: libvpi.nim: Add the getUserData() based retrieval of args

--- count_args/libvpi.nim
+++ count_args/libvpi.nim
@@ -1,14 +1,26 @@
 import std/[strformat]
 import svvpi
 
-vpiDefine task count_args:
-  ## Count the number of arguments to the calling VPI task/function.
-  calltf:
-    var
-      argCount = 0
-    for _, _ in systfHandle.vpiArgs(checkError = true):
-      inc argCount
-    vpiEcho &"{tfName} on line {vpi_get(vpiLineNo, systfHandle)} has {argCount} arguments."
+when defined(inefficient):
+  static:
+    echo "Compiling the inefficient version .."
+
+  vpiDefine task count_args:
+    ## Count the number of arguments to the calling VPI task/function.
+    calltf:
+      var
+        argCount = 0
+      for _, _ in systfHandle.vpiArgs(checkError = true):
+        inc argCount
+      vpiEcho &"{tfName} on line {vpi_get(vpiLineNo, systfHandle)} has {argCount} arguments."
+
+else:
+  import ../common
+
+  vpiDefine task count_args:
+    ## Count the number of arguments to the calling VPI task/function.
+    calltf:
+      vpiEcho &"{tfName} on line {vpi_get(vpiLineNo, systfHandle)} has {systfHandle.getUserData().args.len} arguments."
 
 
 setVlogStartupRoutines(count_args)
